70380a0b9724baa0c3adf1bf88a35024439fe055
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-12-16 14:39:52 +0530

    Add project files.

---
+++ DemoGitGroup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoGitGroup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
